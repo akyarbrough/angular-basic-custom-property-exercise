c2affeb697abad0ac5093f60ea755fb4cc53266d
akyarbrough <ayarbrough0816@gmail.com>
2022-10-03 20:33:26 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myPosts:string[] = ["Post 1", "Post 2"]
+}
+
+
+
